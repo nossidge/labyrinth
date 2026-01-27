e8515694f5084914b8d2d86a19462d70964438c2
Paul Thompson <nossidge@gmail.com>
2026-01-27 23:27:24 +0000

    tyger

--- labyrinth/common/labyrinthPages.js
+++ labyrinth/common/labyrinthPages.js
@@ -26,6 +26,7 @@ const labyrinthPages = [
   "../../corridors/swapping-circles/index.html",
   "../../corridors/symbolizm/index.html",
   "../../corridors/trine/index.html",
+  "../../corridors/tyger/index.html",
   "../../corridors/vertical-lines/index.html",
   "../../corridors/withnail/index.html",
   "../../corridors/zeno/index.html",

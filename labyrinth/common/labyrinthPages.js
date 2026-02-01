6fd9eb658f696202f176977f76d82866da86ab32
Paul Thompson <nossidge@gmail.com>
2026-02-01 03:29:24 +0000

    cloudflare

--- labyrinth/common/labyrinthPages.js
+++ labyrinth/common/labyrinthPages.js
@@ -7,6 +7,7 @@ const labyrinthPages = [
   "../../corridors/baby-god/index.html",
   "../../corridors/blade-runner/index.html",
   "../../corridors/bsod/index.html",
+  "../../corridors/cloudflare/index.html",
   "../../corridors/cow/index.html",
   "../../corridors/crash-test-dummies/index.html",
   "../../corridors/dancing-baby/index.html",

1444d52efd0882378e4a5c92ce1232acfb827da1
Paul Thompson <nossidge@gmail.com>
2026-02-04 21:47:14 +0000

    parklife

--- labyrinth/common/labyrinthPages.js
+++ labyrinth/common/labyrinthPages.js
@@ -20,6 +20,7 @@ const labyrinthPages = [
   "../../corridors/in-utero/index.html",
   "../../corridors/mompster-quest/index.html",
   "../../corridors/othello/index.html",
+  "../../corridors/parklife/index.html",
   "../../corridors/piano/index.html",
   "../../corridors/pills/index.html",
   "../../corridors/poem-fragments/index.html",

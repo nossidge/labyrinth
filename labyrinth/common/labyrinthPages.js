8cdba8771f099ce2593e72ddf3511053695eb031
Paul Thompson <nossidge@gmail.com>
2026-02-08 12:37:16 +0000

    symbolizmo

--- labyrinth/common/labyrinthPages.js
+++ labyrinth/common/labyrinthPages.js
@@ -35,6 +35,7 @@ const labyrinthPages = [
   "../../corridors/swapping-circles/index.html",
   "../../corridors/symbolizm/index.html",
   "../../corridors/symbolizma/index.html",
+  "../../corridors/symbolizmo/index.html",
   "../../corridors/trine/index.html",
   "../../corridors/tyger/index.html",
   "../../corridors/vertical-lines/index.html",
